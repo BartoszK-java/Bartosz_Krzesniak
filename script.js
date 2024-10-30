27e71ea73ca8a8f4d58be6565779f321bd534220
Bartosz <bkrzesniak373@gmail.com>
2024-10-30 10:43:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $('.thumbnail').hover(
         function() {
             $(this).find('img').css('transform', 'scale(1.5)');
-            $(this).find('.description').text($(this).data('desc')).show();
+            $(this).find('.description').text($(this).data('desc')).css('opacity', '1');
         },
         function() {
             $(this).find('img').css('transform', 'scale(1)');
-            $(this).find('.description').hide();
+            $(this).find('.description').css('opacity', '0');
         }
     );
 });
