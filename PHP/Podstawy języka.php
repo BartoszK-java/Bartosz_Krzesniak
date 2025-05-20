bf6dd660dbb3b0ef18d54b275891235189fb578b
Bartosz <bkrzesniak373@gmail.com>
2025-05-20 09:20:00 +0200

    Add files via upload

---
+++ PHP/Podstawy języka.php
@@ -0,0 +1,9 @@
+<?php
+echo "Witaj w świecie PHP!<br>";
+
+$imie = "Bartosz";
+$wiek = 18;
+
+echo "Imię: $imie<br>";
+echo "Wiek: $wiek<br>";
+?>
---
+++ PHP/Struktury kontrolne.php
@@ -0,0 +1,13 @@
+<?php
+$liczba = 11;
+
+if ($liczba > 0) {
+    echo "Liczba jest dodatnia.<br>";
+} else {
+    echo "Liczba nie jest dodatnia.<br>";
+}
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "pętla nr: $i<br>";
+}
+?>
